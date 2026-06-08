ada17a5ccc11f72496595cf477b3284a4962b1d2
Miko Kortelainen <burabotti@gmail.com>
2026-06-09 01:12:56 +0300

    add sorting select component

--- frontend/src/apis.ts
+++ frontend/src/apis.ts
@@ -1,7 +1,7 @@
 import { type VipunenData } from "./types.gen";
 
-export async function getStatistics(): Promise<VipunenData[]> {
-  const res = await fetch("/api/statistics");
+export async function getStatistics(sortQ?: string): Promise<VipunenData[]> {
+  const res = await fetch(`/api/statistics?order=${sortQ || "asc"}`);
   if (!res.ok) throw new Error("failed to fetch statistics");
   return res.json();
 }
--- frontend/src/features/list/DegreeListPage.tsx
+++ frontend/src/features/list/DegreeListPage.tsx
@@ -1,21 +1,33 @@
 import { Stack, Center, Text, HStack, IconButton, ButtonGroup } from "@chakra-ui/react";
 import { Pagination } from "@chakra-ui/react";
 import SchoolCard from "./SchoolCard";
+import SortControl, { type SortOption } from "./SortControl";
 import { useQuery } from "@tanstack/react-query";
 import { getStatistics } from "../../apis";
 import { useState } from "react";
+import type { VipunenData } from "../../types.gen";
 
 const PAGE_SIZE = 10;
 
 export default function DegreeListPage() {
   const [page, setPage] = useState(1);
-  const query = useQuery({ queryKey: ["statistics"], queryFn: getStatistics, refetchOnWindowFocus: false });
+  const [sortOrder, setSortOrder] = useState<SortOption>("asc");
+  const query = useQuery<VipunenData[]>({
+    queryKey: ["statistics", sortOrder],
+    queryFn: () => getStatistics(sortOrder),
+    refetchOnWindowFocus: false,
+    staleTime: Infinity, // old: 30 * 60 * 1000
+    gcTime: 10 * 60 * 1000, // 10 min
+  });
 
   const paginated = query.data?.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
 
   return (
     <Center h="100%" px={8}>
       <Stack direction="column" gap={4} width="800px" p={2}>
+        <HStack justify="space-between">
+          <SortControl value={sortOrder} onChange={setSortOrder} />
+        </HStack>
         {query.isPending ? <Text>Fetching statistics..</Text> : null}
         {query.isError ? <Text>Error fetching statistics..</Text> : null}
 
