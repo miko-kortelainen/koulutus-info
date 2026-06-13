c183ba212d457e390cbc6475c038c9163ab5bf2f
Miko Kortelainen <burabotti@gmail.com>
2026-06-13 03:21:50 +0300

    improve vipunen data merging logic, adding filter for valintatapaJononTyyppi

--- backend/models/vipunen.go
+++ backend/models/vipunen.go
@@ -1,13 +1,14 @@
 package models
 
 type VipunenData struct {
-	KooditHakukohde        string `json:"kooditHakukohde"`
-	Hakukohde              string `json:"hakukohde"`
-	Korkeakoulu            string `json:"korkeakoulu"`
-	KoulutuksenKieli       string `json:"koulutuksenKieli"`
-	Sektori                string `json:"sektori"`
-	KoulutusAla            string `json:"koulutusalaTaso1"`
-	AloituspaikatLkm       int    `json:"aloituspaikatLkm"`
-	KaikkiHakijatLkm       int    `json:"kaikkiHakijatLkm"`
-	EnsisijaisetHakijatLkm int    `json:"ensisijaisetHakijatLkm"`
+	KooditHakukohde        string  `json:"kooditHakukohde"`
+	Hakukohde              string  `json:"hakukohde"`
+	Korkeakoulu            string  `json:"korkeakoulu"`
+	KoulutuksenKieli       string  `json:"koulutuksenKieli"`
+	Sektori                string  `json:"sektori"`
+	KoulutusAla            string  `json:"koulutusalaTaso1"`
+	ValintatapajononTyyppi *string `json:"valintatapajononTyyppi,omitempty"`
+	AloituspaikatLkm       int     `json:"aloituspaikatLkm"`
+	KaikkiHakijatLkm       int     `json:"kaikkiHakijatLkm"`
+	EnsisijaisetHakijatLkm int     `json:"ensisijaisetHakijatLkm"`
 }
--- backend/services/vipunen_cache.go
+++ backend/services/vipunen_cache.go
@@ -19,8 +19,6 @@ var VipunenCache = VipunenDataCache{
 
 var allOrders = []Order{OrderAsc, OrderDesc, OrderMostPopular, OrderLeastPopular, OrderMostSpots, OrderLeastSpots}
 
-const VipunenURL = "https://api.vipunen.fi/api/resources/korkeakoulutus_hakeneet_ja_paikan_vastaanottaneet/data?filter=koulutuksenAlkamisvuosi%3D%3D%272026%27%20and%20hakutapa%3D%3D%27Yhteishaku%27%20and%20koulutusasteTaso1%3D%3D%27Alempi%20korkeakouluaste%27"
-
 func GetVipunenDataCached(order Order) ([]models.VipunenData, error) {
 	VipunenCache.mu.RLock()
 	dataExists := len(VipunenCache.Sorted) > 0
@@ -34,7 +32,7 @@ func GetVipunenDataCached(order Order) ([]models.VipunenData, error) {
 		fmt.Println("/api/statistics/: data not in cache or it's stale, requesting new...")
 
 		// get fresh data from vipunen API
-		freshData, err := FetchVipunenData(VipunenURL)
+		freshData, err := FetchVipunenData(buildVipunenURL())
 		if err != nil {
 			return nil, err
 		}
