2dd6d8233aca456d4530ad8413c73fe6b0024cc5
miko <burabotti@gmail.com>
2026-06-24 23:31:49 +0300

    Include Link response headers for agent discovery (RFC 8288)

--- frontend/src/pages/+Head.tsx
+++ frontend/src/pages/+Head.tsx
@@ -1,9 +1,11 @@
 import favicon from "../../public/favicon.svg";
 
 export function Head() {
-  <>
-    <link rel="icon" href={favicon} type="image/svg+xml" />;
-    <meta name="description" content="Tutki korkeakoulujen yhteishaun hakijamääriä, trendejä sekä koulutustarjontaa." />
-  </>;
-  return;
+  return (
+    <>
+      <link rel="icon" href={favicon} type="image/svg+xml" />
+      <meta name="description" content="Tutki korkeakoulujen yhteishaun hakijamääriä, trendejä sekä koulutustarjontaa." />
+      <link rel="api-catalog" href="/.well-known/api-catalog" />
+    </>
+  );
 }
