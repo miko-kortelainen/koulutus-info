d4544d8dd61f04efc61682553499e89172f717a4
miko <burabotti@gmail.com>
2026-06-24 23:10:07 +0300

    change links to a tags

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,28 +1,24 @@
 import { Box, Link, Separator, Stack } from "@chakra-ui/react";
-import { navigate } from "vike/client/router";
 
 export default function Header() {
-  const landing = () => navigate("/");
-  const stats = () => navigate("/hakijamaarat");
-  const schools = () => navigate("/koulutukset");
-  // const trends = () => navigate("/trendit");
-
   return (
     <Box p={3} px={6}>
       <Stack direction="row" justifyContent={{ base: "center", md: "left" }} fontSize={{ base: "lg", md: "xl" }}>
-        <Link onClick={landing}>etusivu</Link>
+        <Link>
+          <a href="/">etusivu</a>
+        </Link>
 
         <Separator orientation="vertical" />
 
-        <Link onClick={stats}>hakijamäärät</Link>
+        <Link>
+          <a href="/hakijamaarat">hakijamäärät</a>
+        </Link>
 
         <Separator orientation="vertical" />
 
-        <Link onClick={schools}>koulutukset</Link>
-
-        {/* <Separator orientation="vertical" />
-
-        <Link onClick={trends}>trendit</Link> */}
+        <Link>
+          <a href="/koulutukset">koulutukset</a>
+        </Link>
       </Stack>
     </Box>
   );
--- frontend/src/pages/+Head.tsx
+++ frontend/src/pages/+Head.tsx
@@ -1,5 +1,9 @@
 import favicon from "../../public/favicon.svg";
 
 export function Head() {
-  return <link rel="icon" href={favicon} type="image/svg+xml" />;
+  <>
+    <link rel="icon" href={favicon} type="image/svg+xml" />;
+    <meta name="description" content="Tutki korkeakoulujen yhteishaun hakijamääriä, trendejä sekä koulutustarjontaa." />
+  </>;
+  return;
 }
