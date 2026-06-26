ac09f13d11d59d73a0fee36cd809c7fbb66ad38c
miko <burabotti@gmail.com>
2026-06-27 00:40:11 +0300

    add privacy policy page

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,11 +1,16 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Link, Stack, Text } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
     <Box as="footer" p={2} textAlign="center">
-      <Text fontSize={{ base: "2xs", md: "xs" }} color="gray">
-        Lähteet: Vipunen - opetushallinnon tilastopalvelu & Opintopolku.fi
-      </Text>
+      <Stack gap={1} alignItems="center">
+        <Text fontSize={{ base: "2xs", md: "xs" }} color="gray">
+          Lähteet: Vipunen - opetushallinnon tilastopalvelu & Opintopolku.fi
+        </Text>
+        <Link href="/tietosuojaseloste" fontSize={{ base: "2xs", md: "xs" }} color="gray">
+          Tietosuojaseloste
+        </Link>
+      </Stack>
     </Box>
   );
 }
