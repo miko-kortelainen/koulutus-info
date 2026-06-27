a3f64b317fdf28a784ef35d8425b720069dfe467
miko <burabotti@gmail.com>
2026-06-27 20:39:12 +0300

    fix: remove query params from opintopolku fetching to return correct toteutukset.

--- backend/services/opintopolku_url.go
+++ backend/services/opintopolku_url.go
@@ -9,22 +9,14 @@ import (
 // Opintopolku API query parameters
 const (
 	OpintopolkuBaseURL = "https://opintopolku.fi/konfo-backend/external/search/koulutukset"
-
-	OpintopolkuParamKoulutustyyppi = "amk-alempi,kandi-ja-maisteri"
-	OpintopokuParamValintatapa     = "valintatapajono_kp,valintatapajono_tv,valintatapajono_yp"
 )
 
 // construct the Opintopolku API URL from the parameters.
 func BuildOpintopolkuURL(yhteishakuOID string, alkamisajankohdat []string) (string, string, error) {
 	u, _ := url.Parse(OpintopolkuBaseURL)
 	query := url.Values{
-		"hakutapa":           {"hakutapa_01"},
 		"size":               {"100"},
 		"lng":                {"fi"},
-		"opetustapa":         {"opetuspaikkakk_1,opetuspaikkakk_2,opetuspaikkakk_3,opetuspaikkakk_4"},
-		"maksullisuustyyppi": {"maksuton"},
-		"koulutustyyppi":     {OpintopolkuParamKoulutustyyppi},
-		"valintatapa":        {OpintopokuParamValintatapa},
 	}
 
 	trimmedOID := strings.TrimSpace(yhteishakuOID)
