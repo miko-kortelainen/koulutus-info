1e4f0d27983a36896a86ebbb25ddf17003631ae4
miko <burabotti@gmail.com>
2026-07-06 03:44:37 +0300

    feat: add logo image and enhance LandingPage layout with improved text and component structure

--- frontend/src/pages/index/+Page.tsx
+++ frontend/src/pages/index/+Page.tsx
@@ -1,4 +1,4 @@
-import { Badge, Button, Center, Group, Heading, Stack, Text, VStack } from "@chakra-ui/react";
+import { Badge, Button, Center, Heading, HStack, Image, Stack, Text, VStack } from "@chakra-ui/react";
 import { HiCursorClick } from "react-icons/hi";
 import { navigate } from "vike/client/router";
 import { COLORS } from "../../theme";
@@ -11,35 +11,38 @@ export default function LandingPage() {
   return (
     <>
       <Center flex={1}>
-        <Stack gap={{ base: "10", md: "20" }} px="10" textAlign="center" alignItems="center">
-          <Stack gap={{ base: "6", md: "12" }}>
-            <Heading as="h1" size={{ base: "3xl", md: "5xl" }} fontWeight="bold" letterSpacing="widest">
-              yhteishaku.app
-            </Heading>
+        <Stack gap={{ base: "14", md: "20" }} px="10" textAlign="center" alignItems="center">
+          <Stack gap={{ base: "4", md: "7" }}>
+            <VStack>
+              <Image src="/images/logo.png" alt="yhteishaku.app" boxSize={{ base: "16", md: "28" }} />
+              <Heading as="h1" size={{ base: "3xl", md: "5xl" }} fontWeight="bold" letterSpacing="widest">
+                yhteishaku.app
+              </Heading>
+            </VStack>
             <VStack>
               <Text fontSize={{ base: "md", md: "xl" }} fontWeight="semibold" textWrap="pretty">
-                Selaa korkeakoulujen yhteishaun
+                Selaa ja vertaile korkeakoulujen yhteishaun
               </Text>
-              <Group>
-                <Badge size={{ base: "md", md: "lg" }} bg={COLORS.accent} color={COLORS.text}>
+              <HStack justifyContent="center" gap={4} width="100%">
+                <Badge letterSpacing="wide" size={{ base: "md", md: "lg" }} bg={COLORS.accent} color={COLORS.text}>
                   Hakijamääriä
                 </Badge>
 
-                <Badge size={{ base: "md", md: "lg" }} bg={COLORS.accent} color={COLORS.text}>
+                <Badge letterSpacing="wide" size={{ base: "md", md: "lg" }} bg={COLORS.accent} color={COLORS.text}>
                   Trendejä
                 </Badge>
 
-                <Badge size={{ base: "md", md: "lg" }} bg={COLORS.accent} color={COLORS.text}>
+                <Badge letterSpacing="wide" size={{ base: "md", md: "lg" }} bg={COLORS.accent} color={COLORS.text}>
                   Koulutuksia
                 </Badge>
-              </Group>
+              </HStack>
             </VStack>
           </Stack>
 
           <Button
             size={{ base: "xs", md: "lg" }}
             onClick={navigateToStats}
-            width={{ base: "100%" }}
+            width={{ base: "80%" }}
             letterSpacing="wide"
             bgColor={COLORS.accent}
             color={COLORS.text}
