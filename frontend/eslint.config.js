910fc881fcb9bdd9e7ed3619ec81bf4ddccc7fe1
miko <burabotti@gmail.com>
2026-07-08 21:23:07 +0300

    feat: migrate to biome

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,23 +7,26 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist", "src/components/ui/"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
-    plugins: {
-      react: reactPlugin,
-    },
-    rules: {
-      "react/jsx-curly-brace-presence": ["warn", { props: "never", children: "never" }],
-    },
-  },
+	globalIgnores(["dist", "src/components/ui/"]),
+	{
+		files: ["**/*.{ts,tsx}"],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			globals: globals.browser,
+		},
+		plugins: {
+			react: reactPlugin,
+		},
+		rules: {
+			"react/jsx-curly-brace-presence": [
+				"warn",
+				{ props: "never", children: "never" },
+			],
+		},
+	},
 ]);
