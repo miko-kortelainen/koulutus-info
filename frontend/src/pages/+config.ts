3fca9c129d2ed2a2cfa28874c4166e88c6192509
miko <burabotti@gmail.com>
2026-07-09 19:44:16 +0300

    feat: update sitemap change frequency to weekly and enhance page title for clarity

--- frontend/src/pages/+config.ts
+++ frontend/src/pages/+config.ts
@@ -2,7 +2,7 @@ import type { Config } from "vike/types";
 import vikeReact from "vike-react/config";
 
 export default {
-  title: "Yhteishaku.app – hakijamäärät ja koulutukset",
+  title: "Yhteishaun 2026 hakijamäärät ja koulutukset | Yhteishaku.app",
   prerender: true,
   extends: vikeReact,
   htmlAttributes: { lang: "fi" },
