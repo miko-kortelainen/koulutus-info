37470d8ac22acb510341273d066d4edd13a6942f
miko <burabotti@gmail.com>
2026-07-22 19:50:17 +0300

    add support for ios pwa

--- frontend/src/pages/+Head.tsx
+++ frontend/src/pages/+Head.tsx
@@ -2,12 +2,15 @@ export function Head() {
   return (
     <>
       <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+      <link href="/images/180x180.png" rel="apple-touch-icon" sizes="180x180" />
+      <link href="/manifest.webmanifest" rel="manifest" />
       <link href="https://fonts.googleapis.com" rel="preconnect" />
       <link crossOrigin="" href="https://fonts.gstatic.com" rel="preconnect" />
       <link
         href="https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wdth,wght@6..144,25..151,1..1000&display=swap"
         rel="stylesheet"
       />
+      <meta content="#80aa2a" name="theme-color" />
       <meta content="website" property="og:type" />
       <meta content="https://yhteishaku.app/images/og-img.png" property="og:image" />
       <meta content="summary_large_image" name="twitter:card" />
